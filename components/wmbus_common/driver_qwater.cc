75097343d69695d9fb80810f9b0e00811b59bea3
Sezer K. <11984325+darkson95@users.noreply.github.com>
2025-10-17 11:27:25 +0200

    Update driver_qwater.cc

--- components/wmbus_common/driver_qwater.cc
+++ components/wmbus_common/driver_qwater.cc
@@ -50,7 +50,6 @@ namespace
         di.addDetection(MANUFACTURER_QDS, 0x07,  0x19);
         di.addDetection(MANUFACTURER_QDS, 0x06,  0x35);
         di.addDetection(MANUFACTURER_QDS, 0x07,  0x35);
-        di.usesProcessContent();
 
         di.setConstructor([](MeterInfo& mi, DriverInfo& di){ return std::shared_ptr<Meter>(new Driver(mi, di)); });
     });
